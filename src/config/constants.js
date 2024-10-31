27951f4ab5dd15159f3ce0dd0d1a8c63de46105a
kimgaaaa <dl4525@naver.com>
2024-10-31 17:57:38 +0900

    API 주소변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL="http://localhost:8080"
+export const API_URL= process.env.NODE_ENV==='production'? "https://port-0-pet-server-m2x0x21s4f696599.sel4.cloudtype.app" : "http://localhost:8080" ;
