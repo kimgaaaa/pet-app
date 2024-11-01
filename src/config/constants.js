48da310264ee696da665ed5f6bc6bc6f126c6d8d
kimgaaaa <dl4525@naver.com>
2024-11-01 17:45:26 +0900

    최최종

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL= process.env.NODE_ENV==='production'? "https://port-0-pet-server-m2x0x21s4f696599.sel4.cloudtype.app" : "http://localhost:8080" ;
+export const API_URL= process.env.NODE_ENV==='production'? "https://port-0-pet-server-m2yhjxjlaf4809b2.sel4.cloudtype.app" : "http://localhost:8080" ;
