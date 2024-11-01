709b893c033607ae9cd85e175a6691cb8cab47ba
kimgaaaa <dl4525@naver.com>
2024-11-01 18:07:09 +0900

    mmm

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL= process.env.NODE_ENV==='production'? "https://port-0-pet-server-m2yhjxjlaf4809b2.sel4.cloudtype.app" : "http://localhost:8080" ;
+export const API_URL= process.env.NODE_ENV==='production'? "https://port-0-pet-server-m2yhz3j1f17dfd7e.sel4.cloudtype.app" : "http://localhost:8080" ;
